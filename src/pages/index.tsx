d589f0040cd93928ff62e605d0ece63992416281
Mateus <shakii876954@gmail.com>
2024-02-26 20:24:24 -0300

    [feat]:<first commit>

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>idk</title>
+      </Head>
+      <body className="bg-neutral-950 w-screen h-screen">
+        <div className="w-screen h-screen absolute bg-purple-900/10 pointer-events-none"/>
+      </body>
+    </>
+  );
+}
